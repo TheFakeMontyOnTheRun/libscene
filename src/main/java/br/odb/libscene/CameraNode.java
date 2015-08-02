48eb9f52de0d709d2e7752e968fb8caccb89c774
Daniel Monteiro <danielmonteiro@id.uff.br>
2015-08-02 01:23:10 -0300

    New methods for camera nodes
    It now allows you to properly move and view.

--- src/main/java/br/odb/libscene/CameraNode.java
+++ src/main/java/br/odb/libscene/CameraNode.java
@@ -8,25 +8,60 @@ public class CameraNode extends DirectedSceneNode implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 9054020252855916205L;
-	
+
 	public CameraNode(SceneNode other) {
 		super(other);
 
-		if ( other instanceof CameraNode ) {
-			setFrom( (CameraNode) other );	
-		}		
+		if (other instanceof CameraNode) {
+			setFrom((CameraNode) other);
+		}
 	}
 
+	public void onLeft() {
+		angleXZ -= 10;
+	}
+
+	public void onRight() {
+		angleXZ += 10;
+	}
+
+	public void onWalkForward() {
+		localPosition.x += 10 * Math.sin(angleXZ * (Math.PI / 180.0f));
+		localPosition.z -= 10 * Math.cos(angleXZ * (Math.PI / 180.0f));
+	}
+
+	public void onWalkBack() {
+		localPosition.x -= 10 * Math.sin(angleXZ * (Math.PI / 180.0f));
+		localPosition.z += 10 * Math.cos(angleXZ * (Math.PI / 180.0f));
+	}
+
+	public void onMoveDown() {
+		localPosition.y -= 10;
+	}
+	
+	public void onMoveUp() {
+		localPosition.y += 10;
+	}
+	
+	public void onStrafeLeft() {
+        localPosition.x += 10 * Math.sin((angleXZ - 90)* (Math.PI / 180.0f));
+        localPosition.z -= 10 * Math.cos((angleXZ - 90) * (Math.PI / 180.0f));
+	}
+	
+	public void onStrafeRight() {
+        localPosition.x -= 10 * Math.sin((angleXZ - 90)* (Math.PI / 180.0f));
+        localPosition.z += 10 * Math.cos((angleXZ - 90) * (Math.PI / 180.0f));
+	}
 	
 	public CameraNode(String id) {
 		super(id);
 	}
 
-	public CameraNode( SceneNode other, String newId) {
+	public CameraNode(SceneNode other, String newId) {
 		super(other, newId);
 
-		if ( other instanceof CameraNode ) {
-			setFrom( (CameraNode) other );	
-		}	
+		if (other instanceof CameraNode) {
+			setFrom((CameraNode) other);
+		}
 	}
 }
